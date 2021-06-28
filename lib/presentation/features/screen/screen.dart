68540ebf2b0ade6e681f9a7fb9841db5ffef903d
Vitaliklisa <vhomenko119@gmail.com>
2021-06-28 18:30:54 +0300

    Первый костиль

--- lib/presentation/features/screen/screen.dart
+++ lib/presentation/features/screen/screen.dart
@@ -10,37 +10,71 @@ class Screen extends StatefulWidget {
 }
 
 class _ScreenState extends State<Screen> implements ScreenView {
-
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (BuildContext context) => ScreenModel(this),
       child: Consumer<ScreenModel>(
         builder: (context, model, child) {
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: model.backgroundColor,
-              title: Text("My site",
-                style: TextStyle(fontSize: 45.0, color: model.textColor,),),
-              centerTitle: true,
-              actions: [
-                FloatingActionButton(
-                  backgroundColor: model.buttonColor,
-                  onPressed: model.onFabPressed,
-                )
-              ],
-            ),
-            body: Container(
-              alignment: Alignment.center,
-              color: model.backgroundColor,
-              child: Text(
-                'I hate cold',
-                style: TextStyle(fontSize: 45.0, color: model.textColor),
+          return Scaffold(backgroundColor: model.backgroundColor,
+            appBar: buildAppBar(model),
+            body: RawScrollbar(
+              thumbColor: Color(0xFF150A28),
+              isAlwaysShown: true,
+              radius: Radius.circular(20),
+              thickness: 5,
+              child: SingleChildScrollView(
+                child: Stack(
+                  children: [
+                    Column(crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 100),
+                        Image.asset('assets/images/seller.jpg'),
+                        Image.asset('assets/images/anon.jpg'),
+                      ],
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      child: Column(
+                        children: [
+                          SizedBox(height: 200),
+                          Text(
+                            'I hate cold',
+                            style: TextStyle(
+                              fontSize: 45.0,
+                              color: model.textColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           );
         },
       ),
+    );
+  }
+
+  AppBar buildAppBar(ScreenModel model) {
+    return AppBar(
+      backgroundColor: model.backgroundColor,
+      title: Text(
+        "My site",
+        style: TextStyle(
+          fontSize: 45.0,
+          color: model.textColor,
+        ),
+      ),
+      centerTitle: true,
+      actions: [
+        FloatingActionButton(
+          backgroundColor: model.buttonColor,
+          onPressed: model.onFabPressed,
+        )
+      ],
     );
   }
 }
