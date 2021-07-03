09c693a22af9f84b197f1547c49bb2032275f41c
Vitaliklisa <vhomenko119@gmail.com>
2021-07-03 08:47:29 +0300

    Scroll bar

--- lib/presentation/features/screen/screen.dart
+++ lib/presentation/features/screen/screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
@@ -16,65 +17,50 @@ class _ScreenState extends State<Screen> implements ScreenView {
       create: (BuildContext context) => ScreenModel(this),
       child: Consumer<ScreenModel>(
         builder: (context, model, child) {
-          return Scaffold(backgroundColor: model.backgroundColor,
-            appBar: buildAppBar(model),
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: model.backgroundColor,
+              title: Text(
+                "My site",
+                style: TextStyle(
+                  fontSize: 45.0,
+                  color: model.textColor,
+                ),
+              ),
+              centerTitle: true,
+              actions: [
+                FloatingActionButton(
+                  backgroundColor: model.buttonColor,
+                  onPressed: model.onFabPressed,
+                )
+              ],
+            ),
             body: RawScrollbar(
               thumbColor: Color(0xFF150A28),
               isAlwaysShown: true,
               radius: Radius.circular(20),
               thickness: 5,
               child: SingleChildScrollView(
-                child: Stack(
-                  children: [
-                    Column(crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(height: 100),
-                        Image.asset('assets/images/seller.jpg'),
-                        Image.asset('assets/images/anon.jpg'),
-                      ],
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      child: Column(
-                        children: [
-                          SizedBox(height: 200),
-                          Text(
-                            'I hate cold',
-                            style: TextStyle(
-                              fontSize: 45.0,
-                              color: model.textColor,
-                            ),
-                          ),
-                        ],
+                child: Container(
+                  alignment: Alignment.center,
+                  color: model.backgroundColor,
+                  child: Column(
+                    children: [
+                      Text(
+                        'I hate cold',
+                        style:
+                        TextStyle(fontSize: 45.0, color: model.textColor),
                       ),
-                    ),
-                  ],
+                      Image.asset('assets/images/seller.jpg'),
+                      Image.asset('assets/images/anon.jpg'),
+                    ],
+                  ),
                 ),
               ),
             ),
           );
         },
       ),
-    );
-  }
-
-  AppBar buildAppBar(ScreenModel model) {
-    return AppBar(
-      backgroundColor: model.backgroundColor,
-      title: Text(
-        "My site",
-        style: TextStyle(
-          fontSize: 45.0,
-          color: model.textColor,
-        ),
-      ),
-      centerTitle: true,
-      actions: [
-        FloatingActionButton(
-          backgroundColor: model.buttonColor,
-          onPressed: model.onFabPressed,
-        )
-      ],
     );
   }
 }
